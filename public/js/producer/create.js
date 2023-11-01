1ce4f7a48a75132c1da7a724dcb58c038623f9f9
ndeluca <ndeluca@gmu.edu>
2023-11-01 14:18:21 -0400

    some fixes

--- public/js/producer/create.js
+++ public/js/producer/create.js
@@ -49,7 +49,15 @@ function populateDJs() {
 document.getElementById('song-search').addEventListener('input', function (event) {
   event.target.value = event.target.value.replace(/[^a-zA-Z]/g, '');
   const searchTerm = event.target.value.toLowerCase();
-  const matchedSongs = songs.filter(song => song.title.toLowerCase().includes(searchTerm));
+
+  // Filter out songs based on the search term and DJ's song list
+  const selectedDJID = parseInt(document.getElementById('dj-select').value);
+  const dj = djs.find(dj => dj.djID === selectedDJID);
+  
+  const matchedSongs = songs.filter(song => 
+      song.title.toLowerCase().includes(searchTerm) && dj.songs.includes(song.songID)
+  );
+
   displaySongs(matchedSongs);
 });
 
@@ -58,7 +66,12 @@ function displaySongs(matchedSongs) {
   const songList = document.getElementById('song-list');
   songList.innerHTML = ''; // Clear the current list
 
-  matchedSongs.forEach(song => {
+  const selectedDJID = parseInt(document.getElementById('dj-select').value);
+    const dj = djs.find(dj => dj.djID === selectedDJID);
+    
+    const filteredSongs = matchedSongs.filter(song => dj.songs.includes(song.songID));
+
+  filteredSongs.forEach(song => {
     const li = document.createElement('li');
     const input = document.createElement('input');
     const label = document.createElement('label');
@@ -120,5 +133,65 @@ document.querySelector('form').addEventListener('submit', function (event) {
     .catch(error => {
       console.error('Error:', error);
     });
+});
+
+function updateDJOnServer(dj) {
+  // Create a fetch request to update the DJ on the server
+  fetch('/api/djs', {  // Update this endpoint
+      method: 'POST',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(dj) // Send the updated DJ object to the server
+  })
+  .then(response => response.json())
+  .then(data => {
+      if (data.success) {
+          console.log('Successfully updated DJ on server.');
+      } else {
+          console.error('Error updating DJ:', data.error);
+      }
+  });
+}
+
+document.getElementById('add-event-to-dj-btn').addEventListener('click', function() {
+  addEventToDJ();
+});
+
+function addEventToDJ() {
+  // Get the selected timeslot, songs, and DJ details
+  const selectedTimeSlot = document.getElementById('time-slot').value;
+  const selectedDJID = parseInt(document.getElementById('dj-select').value);
+  const selectedDJName = document.getElementById('dj-select').options[document.getElementById('dj-select').selectedIndex].textContent;
+  const selectedSongs = Array.from(document.querySelectorAll('#song-list input:checked'))
+    .map(input => input.value);
+
+  // Create the new event object
+  const newEvent = {
+    dj: selectedDJName,
+    time: selectedTimeSlot,
+    songs: selectedSongs
+  };
+
+  // Find the selected DJ by ID
+  const dj = djs.find(dj => dj.djID === selectedDJID);
+
+  // Add the new event to the DJ's events array
+  if (dj) {
+    dj.events.push(newEvent);
+    console.log(`Event added to ${dj.name}'s event list.`);
+    updateDJOnServer(dj);  // Update the DJ on the server side
+  }
+}
+
+document.getElementById('dj-select').addEventListener('change', function () {
+  // Trigger a search again to filter songs based on the new DJ selection
+  const searchTerm = document.getElementById('song-search').value.toLowerCase();
+  
+  const matchedSongs = songs.filter(song => 
+      song.title.toLowerCase().includes(searchTerm)
+  );
+
+  displaySongs(matchedSongs);
 });
 
